13c90453d0532ea2c220b541edf509da5deeb3b8
melanyvlm <mel.valencia28@outlook.com>
2019-05-09 12:58:18 -0500

    terminando css en página principal

--- visanet-hackaton/src/App.jsx
+++ visanet-hackaton/src/App.jsx
@@ -19,22 +19,22 @@ const App = () => {
               <h4 className="bold"><span className="color-fucsia">SUEÑOS</span> POSIBLES</h4>
               <div className="margin">
               </div>
-              <a href="#" className="bold">CONOCE NUESTROS SERVICIOS</a>
+              <a href="#" className="bold boldy-size">CONOCE NUESTROS SERVICIOS</a>
               </div>
               </div>
             </div>
             <div className="col-6 ">
-            <div class="row justify-content-end">
-          <div class="col-3">
-            <p class="font-weight-bold ">INICIAR SESIÓN</p>
+            <div className="row justify-content-end">
+          <div className="col-3">
+            <p className="font-weight-bold ">INICIAR SESIÓN</p>
           </div>
-          <div class="col-3">
-            <p class="font-weight-bold">REGISTRATE</p>
+          <div className="col-3">
+            <p className="font-weight-bold">REGISTRATE</p>
           </div>
         </div>
             </div>
         </div>
-             <p class="color-fucsia text-center bold m-4">"La plataforma colaborativa que busca el empoderamiento femenino mediante el crecimiento económico de las mujeres emprendedoras"</p>
+             <p className="color-fucsia text-center bold bold-size m-4">"La plataforma colaborativa que busca el empoderamiento femenino mediante el crecimiento económico de las mujeres emprendedoras"</p>
 
      </div>
      </main>
@@ -62,52 +62,54 @@ const App = () => {
 </div>
 </div>
 </div>
-
-
-
-
-{/* SERVICIOS */}
-   <div className="container">
-<div className="row">
-<div className="col-md-6">
-<div className="img7 img"></div>
-
-
-</div>
-<div className="col-md-6">
-<div className="img div-fucsia" >
-<h2 className="color-yellow">PIDE UN PRÉSTAMO PARA TU NEGOCIO</h2>
-<p>Encuentra préstamos a tu medida , sintantos requisitos y de forma rápida</p>
-<button className="yellow-btn">PIDE TU PRÉSTAMO</button>
-</div>
-
-</div>
-</div>
-</div>
-
-    {/* <section>
-      <div className="contenedor">
-      <article>
-    <div className="img1 eq img"></div>
-<h4>HHHKKKS</h4> 
-    </article>
-    <article>
-    <div className="img1 eq img"></div>
-<h4>HHHKKKS</h4> 
-    </article>
-    <article>
-    <div className="img1 eq img"></div>
-<h4>HHHKKKS</h4> 
-    </article>
-      </div>
-    </section> */}
-
-    
+   
 {/* Servicios */}
    <h4 className="text-center m-5 bold">NUESTROS SERVICIOS</h4>
+   <div className="container">
+<div className="card-group">
+<div className="card img7">
+      </div>
+    <div className="card div-fucsia">
+      <div className="card-body mt-5">
+      <div className="ml-5">
+        <h5 className="card-title mt-5 ml-5 color-yellow bold">PIDE UN PRÉSTAMO PARA TU </h5>
+        <h5 className="card-title  ml-5 color-yellow bold"> NEGOCIO</h5>
+        <p className="card-text mt-4 ml-5 color-white">Encuentra préstamos a tu medida, sin tantos
+          requisitos y de forma rápida.</p>
+        <button  href="#" class=" loan-btn ml-5 mt-3 bold">PIDE TU PRÉSTAMO</button>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div className="card-group">
+    <div className="card div-fucsia">
+      <div class="card-body">
+        <h5 class="card-title mt-5 ml-5 color-yellow bold">ENCUENTRA UN PERSONAL DE CONFIANZA</h5>
+        <p class="card-text mt-5 ml-5 color-white">Ingresa tu perfil , cuéntanos qué necesitas (proveedores, colaborados) y te conectamos con la persona ideal para tu negocio</p>
+        <button href="#" class="btn-services color-yellow ml-5 bold">INCREMENTA TU RED DE 
+        <p>CONTACTOS</p></button>
+        
+      </div>
+    </div>
+    <div class="card img9">
+        </div>
+  </div>
+  <div class="card-group">
+<div class="card img8">
+      </div>
+    <div class="card div-fucsia">
+      <div class="card-body">
+        <h5 class="card-title mt-5 ml-5 color-yellow bold">HAZ CRECER TU NEGOCIO</h5>
+        <p class="card-text mt-5 ml-5 color-white"> Te brindamos información del sector de tu negocio y te recomendamos soluciones para hacerlo crecer</p>
+        <button href="#" class="btn-services color-yellow ml-5 mt-3 bold">HAZ CRECER TU NEGOCIO</button>
+      </div>
+    </div>
+  </div>
+  </div>
+
 {/* Tercera sección */}
 
-<h4 className="text-center m-5 bold"> Y TAMBIÉN TE OFRECEMOS SOLUCIONES PARA INCREMENTAR TUS VENTAS</h4>
+<h4 className="text-center m-5 bold black"> Y TAMBIÉN TE OFRECEMOS SOLUCIONES PARA INCREMENTAR TUS VENTAS</h4>
 <div className="container">
 <div className="row">
 
@@ -147,10 +149,7 @@ const App = () => {
 <footer>
   <div className="color-black ">VISANET PERU</div>
 </footer>
-    </>
-
+   </>
   )
-
 } 
-
 export default App;
