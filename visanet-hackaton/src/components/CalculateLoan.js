c3a334539e020844f53cea5db6caa59ba66220b9
cinthyasegura <cinthyaless@gmail.com>
2019-05-09 02:52:36 -0500

    se agrega el nav

--- visanet-hackaton/src/components/CalculateLoan.js
+++ visanet-hackaton/src/components/CalculateLoan.js
@@ -2,16 +2,46 @@ import React from 'react';
 
 const CalculateLoan = ({amount, setAmount, handleChange, calculateValues, value, setViews }) => {
   return (
+    <>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#">Navbar</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Link</a>
+      </li>
+      <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a className="dropdown-item" href="#">Action</a>
+          <a className="dropdown-item" href="#">Another action</a>
+          <div className="dropdown-divider"></div>
+          <a className="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+    <form className="form-inline my-2 my-lg-0">
+      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
     <div>
       <h2>Hola emprendedora</h2>
       <p>Te otorgamos un préstamo para que puedas seguir creciendo</p>
-      <div>
-        <span style={{color: 'red'}}>1</span>
-        <span>2</span>
-        <span>3</span>
-        <span>4</span>
-        <span>5</span>
-      </div>
       <div>
         <span>¿Cuantó dinero necesitas?</span>
         <button 
@@ -43,6 +73,7 @@ const CalculateLoan = ({amount, setAmount, handleChange, calculateValues, value,
         <button type="button" onClick={() => setViews('SecondScreen')}>SOLICITAR</button>
       </div>
     </div> 
+    </>
   )
 };
 
