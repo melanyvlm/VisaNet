8f9d53a8177e29030dfe6bd622d058546b97c0ef
cinthyasegura <cinthyaless@gmail.com>
2019-05-08 22:28:23 -0500

    se muestra la 3ra vista

--- visanet-hackaton/src/components/LoanControl.js
+++ visanet-hackaton/src/components/LoanControl.js
@@ -1,66 +1,45 @@
 import React, { useState } from 'react';
+import ViewLoans from './ViewLoans';
+import CalculateLoan from './CalculateLoan';
 
 const LoanControl = () => {
     const [amount, setAmount] = useState(1000);
     const [value, setValue] = useState('12');
-    const [button, setButton] = useState(false);
-
+    const [views, setViews] = useState('calculateLoan');
+    const [options, setOptions] = useState(false )
 
     const calculateValues = (values) => {
-        const interest = values * 0.12;
+        const interest = values * (Number(value)/100);
         const totalPaid = values + interest;
         const cuote = totalPaid/Number(value);
-        return [interest.toFixed(2), totalPaid.toFixed(2), cuote.toFixed(2)];
+        return [interest, totalPaid.toFixed(2), cuote.toFixed(2)];
     };
 
     const handleChange = (e) => {
         setValue(e.target.value)
     };
 
-    return (
-       <div>
-           <h2>Hola emprendedora</h2>
-           <p>Te otorgamos un préstamo para que puedas seguir creciendo</p>
-            <div>
-                <span style={{color: 'red'}}>1</span>
-                <span>2</span>
-                <span>3</span>
-                <span>4</span>
-                <span>5</span>
-            </div>
-
-           <div>
-               <span>¿Cuantó dinero necesitas?</span>
-               <button 
-                 type="button" 
-                 onClick={() => {
-                    if(amount <= 500 ) {
-                       setAmount(500)
-                    } else {
-                       setAmount(amount - 500)
-                    }
-                 }}> - </button>
-               <span>{amount}</span>
-               <button type="button" onClick={() => setAmount(amount + 500)}> + </button>
-               <span>¿Cuantó tiempo?</span>
-               <select value={value} onChange={handleChange}>
-                   <option value="12">12 MESES</option>
-                   <option value="24">24 MESES</option>
-                   <option value="36">36 MESES</option>
-               </select>
-               <p>Interes: {calculateValues(amount)[0]}</p>
-               <p>Monto a pagar: S/ {calculateValues(amount)[1]}</p>
-               <p>Cuota: {calculateValues(amount)[2]}</p>
-               <span>Prestamo basado en TCEA 12%</span>
-               <div>
-                   <h4>IMPORTANTE</h4>
-                   <div>El préstamo será otorgado por la entidad financiera elegída Juntas y VisaNet
-                       no es responsable de la gestión del crédito.
-                   </div>
-               </div>
-               <button type="button">SOLICITAR</button>
-           </div>
-       </div> 
-    )
+    switch (views) {
+      case 'calculateLoan':
+        return <CalculateLoan 
+          amount={amount}
+          setAmount={setAmount}    
+          handleChange={handleChange}
+          calculateValues={calculateValues}
+          value={value}
+          setViews={setViews}
+        />
+      case 'viewLoans':
+        return <ViewLoans 
+          amount={amount}
+          setAmount={setAmount}
+          handleChange={handleChange}
+          calculateValues={calculateValues}
+          value={value}
+          setViews={setViews}
+        />     
+        default:
+          return <CalculateLoan />
+        }
 };
 export default LoanControl;
---
+++ visanet-hackaton/src/components/ViewLoans.js
@@ -0,0 +1,33 @@
+import React from 'react';
+ 
+const ViewLoans = ({ amount, setAmount, handleChange, calculateValues, value, setViews }) => {
+  return (
+    <div>
+      <button 
+          type="button" 
+          onClick={() => {
+            if(amount <= 500 ) {
+              setAmount(500)
+            } else {
+              setAmount(amount - 500)
+            }
+        }}> - </button>
+      <span>{amount}</span>
+      <button type="button" onClick={() => setAmount(amount + 500)}> + </button>
+      <select value={value} onChange={handleChange}>
+        <option value="12">12 MESES</option>
+        <option value="24">24 MESES</option>
+        <option value="36">36 MESES</option>
+      </select>
+      <span>Tienes un prestamo de: S/ {amount}</span>
+      <span>TCEA: 12%</span>
+      <p>Intereses {calculateValues(amount)[0]} a {value} meses</p>
+      <p>Monto total: S/ {calculateValues(amount)[1]}</p>
+      <p>¿Cuánto voy a pagar? {calculateValues(amount)[2]} mensuales</p>
+      <button type="button" onClick={() => setViews('')}>PIDE TU PRÉSTAMO</button>
+        
+    </div>
+)
+};
+
+export default ViewLoans;
