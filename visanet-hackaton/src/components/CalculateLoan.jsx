a5ea679d0d464dd27b4310f4698301675b303e53
MicaelaSuarezCortez <micaelasuarezco@gmail.com>
2019-05-09 06:26:15 -0500

    estilos a navbar, bienvenida y pasos para el préstamo

--- visanet-hackaton/src/components/CalculateLoan.jsx
+++ visanet-hackaton/src/components/CalculateLoan.jsx
@@ -7,50 +7,45 @@ const CalculateLoan = ({ amount, setAmount, handleChange, calculateValues, value
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className ="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <a className="navbar-brand" href="#" >MUJER EMPRENDEDORA</a>      
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">PIDE PRÉSTAMO</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">RED DE CONTACTOS</a>
             </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-        </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
+            <li className="nav-item">
+              <a className="nav-link" href="#">HAZ CRECER TU NEGOCIO</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">INICIAR SESION</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">REGÍSTRATE</a>
             </li>
           </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
         </div>
       </nav>
       ,
-    <div className="">
-        <h2 className="h2">Hola emprendedora</h2>
-        <p className="p">Te otorgamos un préstamo para que puedas seguir creciendo</p>
+    <div>
+        <div className="content-bienvenida">
+          <h2 className="h2">Hola emprendedora</h2>
+          <p className="p">Te otorgamos un préstamo para que puedas seguir creciendo</p>
+        </div>
         <div>
-          <span style={{ color: 'red' }}>1</span>
-          <span>2</span>
-          <span>3</span>
-          <span>4</span>
-          <span>5</span>
+          <div className="content-pasos-prestamo">
+            <span style={{ color: 'white' }} className="paso1">1</span>
+            <span className="paso2">2</span>
+            <span className="paso3">3</span>
+            <span className="paso4">4</span>
+            <span className="paso5">5</span>
+          </div>
         </div>
         <div>
           <span>¿Cuantó dinero necesitas?</span>
