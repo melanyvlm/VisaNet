2cf65c4cda9f75fc6390c52246b8dc4f4432a921
cinthyasegura <cinthyaless@gmail.com>
2019-05-09 01:43:35 -0500

    se agrega firebase al project

--- visanet-hackaton/src/components/SequenceController.js
+++ visanet-hackaton/src/components/SequenceController.js
@@ -1,23 +1,34 @@
 import React from 'react';
 
 const SequenceController = ({ sections, actualSection }) => {
-    const components = [];
-    for (let index = 0; index < sections; index++) {
-        components.push(
-            <li key={index}>
-                {index + 1}
-                {index < actualSection && '+'}
-                {index > actualSection && '--'}
-                {index === actualSection && 'aqui'}
-            </li>
-        )
-        
-    }
-    return (
-        <ul>{components}</ul>
+  
+
+  const components = [];
+  for (let index = 0; index < sections; index++) {
+    const getStyle = () => {
+        if(index < actualSection) {
+            return {
+                color: 'red'
+            }
+        } else if (index === actualSection) {
+            return {
+                color: 'blue'
+            }
+        } else {
+            return {
+                color: 'black'
+            }
+        }
+      };
+    components.push(
+      <li key={index} style={getStyle()}>
+        {index + 1}  
+      </li>
     )
-    
-    
+  }
+  return (
+    <ul>{components}</ul>
+  )   
 };
 
 export default SequenceController;
