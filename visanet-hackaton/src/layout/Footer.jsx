a367c6baa06c2c4f8f4efea4677104a004064722
cinthyasegura <cinthyaless@gmail.com>
2019-05-10 04:41:06 -0500

    sticky footer

--- visanet-hackaton/src/layout/Footer.jsx
+++ visanet-hackaton/src/layout/Footer.jsx
@@ -2,15 +2,9 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer style={style}className="pt-5">
+    <footer className="pt-5 fixed-bottom">
       <div className="color-black">VISANET PERU</div>
     </footer>
   )
 };
 export default Footer;
-
-const style = {
-    position: 'absolut',
-    bottom: 0,
-
-}
